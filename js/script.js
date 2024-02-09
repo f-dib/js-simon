ed3f891718faccc35388865f6c7106c54ff1d37d
federico-dibitonto <f.dibitonto94@gmail.com>
2024-02-09 18:21:51 +0100

    to fix: set the countdown

---
+++ js/script.js
@@ -0,0 +1,25 @@
+let nextDate = new Date("Feb 12, 2024 09:30:00").getTime();
+
+let updateCount = setInterval(timer(), 1000);
+
+function timer() {
+
+    let resultElement = document.querySelector("#result");
+    let now = new Date().getTime();
+  
+    let timeDifference = nextDate - now;
+  
+    let days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+    resultElement.innerHTML = days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s ";
+
+    if (timeDifference < 0) {
+      clearInterval(updateCount);
+      resultElement.innerHTML = "IT'S TIME";
+    }
+};
+
