cc4f6d1bae72c605a44503770680fc3d5e5d1030
federico-dibitonto <f.dibitonto94@gmail.com>
2024-02-11 18:59:17 +0100

    exercise completed: fixed style

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 let nextDate = new Date("Feb 12, 2024 09:30:00").getTime();
 let buttonElement = document.querySelector('#btn');
 
+
 buttonElement.addEventListener("click", 
     function() {
         
@@ -11,7 +12,10 @@ buttonElement.addEventListener("click",
 
 function timer() {
 
-    let resultElement = document.querySelector("#result");
+    let dayElement = document.querySelector('#day');
+    let hourElement = document.querySelector('#hour');
+    let minuteElement = document.querySelector('#minute');
+    let secondElement = document.querySelector('#second');
     let now = new Date().getTime();
   
     let timeDifference = nextDate - now;
@@ -19,9 +23,17 @@ function timer() {
     let days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
     let hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     let minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
-    let seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+    let seconds = String(Math.floor((timeDifference % (1000 * 60)) / 1000));
 
-    resultElement.innerHTML = days + "d " + hours + "h "+ minutes + "m " + seconds + "s ";
+    dayElement.innerHTML = days;
+    hourElement.innerHTML = hours;
+    minuteElement.innerHTML = minutes;
+    secondElement.innerHTML = seconds;
+    
+
+    if (seconds.length < 2) {
+        seconds = 0 + seconds;
+    }
 
     if (timeDifference < 0) {
       clearInterval(updateCount);
