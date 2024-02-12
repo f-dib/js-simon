21386966f3f9c5f15ed0c8a50527489c50988038
federico-dibitonto <f.dibitonto94@gmail.com>
2024-02-12 01:48:22 +0100

    exercise completed: fix style font and size element

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
-let nextDate = new Date("Feb 12, 2024 09:30:00").getTime();
+
+
+let nextDate = new Date("Feb 12, 2024 01:44:00").getTime();
 let buttonElement = document.querySelector('#btn');
 
 
@@ -12,6 +14,7 @@ buttonElement.addEventListener("click",
 
 function timer() {
 
+    let resultElement = document.querySelector('#result');
     let dayElement = document.querySelector('#day');
     let hourElement = document.querySelector('#hour');
     let minuteElement = document.querySelector('#minute');
@@ -23,21 +26,22 @@ function timer() {
     let days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
     let hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     let minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
-    let seconds = String(Math.floor((timeDifference % (1000 * 60)) / 1000));
+    let seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
 
     dayElement.innerHTML = days;
     hourElement.innerHTML = hours;
     minuteElement.innerHTML = minutes;
     secondElement.innerHTML = seconds;
     
-
-    if (seconds.length < 2) {
-        seconds = 0 + seconds;
-    }
+    // to fix
+    // if (dayElement < 10) {
+    //     dayElement = "0" + dayElement;
+    // }
 
     if (timeDifference < 0) {
       clearInterval(updateCount);
-      resultElement.innerHTML = "IT'S TIME";
+      resultElement.style.color = 'red';
+      resultElement.innerHTML = "THE END IT'S NEAR";
     }
 };
 
